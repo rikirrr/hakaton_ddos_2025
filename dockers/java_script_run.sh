1d728e4d1c1aeb356e189265bb63df8165835fe4
ynovka <kirill.janovich@gmail.com>
2025-09-26 15:33:48 +0300

    4

--- dockers/java_script_run.sh
+++ dockers/java_script_run.sh
@@ -2,7 +2,7 @@
 
 set -e
 if [ ! -f package.json ]; then
-  echo "❌ package.json не найден!"
+  echo "ackage.json не найден!"
   exit 1
 fi
 
@@ -13,21 +13,21 @@ if grep -q '"next"' package.json; then
   npm run build
   exec npm run start
 elif grep -q '"nuxt"' package.json; then
-  echo "➡️ Nuxt.js проект"
+  echo "Nuxt.js проект"
   npm run build
   exec npm run start
 elif grep -q '"@angular/core"' package.json; then
-  echo "➡️ Angular проект"
+  echo "Angular проект"
   exec npm run start
 elif grep -q '"vue"' package.json; then
-  echo "➡️ Vue.js проект"
+  echo "Vue.js проект"
   npm run build
   exec npm run preview || npm run serve
 elif grep -q '"react"' package.json; then
-  echo "➡️ React проект"
+  echo "React проект"
   npm run build
   exec npm run start || npx serve -s build
 else
-  echo "❓ Неизвестный проект, просто запускаю npm start"
+  echo "Неизвестный проект, просто запускаю npm start"
   exec npm start
 fi
--- dockers/python_run.sh
+++ dockers/python_run.sh
@@ -2,16 +2,16 @@
 set -e
 
 if [ -f requirements.txt ]; then
-    echo ">>> Installing via pip requirements.txt"
+    echo "Установка через pip requirements.txt"
     pip install -r requirements.txt
 
 elif [ -f pyproject.toml ]; then
-    echo ">>> Installing via poetry"
+    echo "Установка через poetry"
     pip install poetry
     poetry install --no-root
 
 elif [ -f environment.yml ]; then
-    echo ">>> Installing via conda environment.yml"
+    echo "Установка через conda environment.yml"
     apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
     curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
     bash miniconda.sh -b -p /opt/conda
